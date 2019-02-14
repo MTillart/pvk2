989b4e92946bee648cb0620021db45a48fbceb5e
MTillart <merliti.tillart@khk.ee>
2019-02-14 09:05:00 +0200

    Loodud rakenduse pea mall ja täidetud test sisuga

--- menu/index.php
+++ menu/index.php
@@ -1,8 +1,16 @@
 <?php
 require_once 'conf.php';
 
-echo '<pre>';
-print_r($sess);
-echo '</pre>';
+//echo '<pre>';
+//print_r($sess);
+//echo '</pre>';
+
+$mainTmp = new Template('main');
+
+$mainTmp->set('title', 'Menu Application');
+$mainTmp->set('page title', 'Tartu KHK Menu');
+$mainTmpContent = $mainTmp->parse();
+
+echo $mainTmpContent;
 
 ?>
